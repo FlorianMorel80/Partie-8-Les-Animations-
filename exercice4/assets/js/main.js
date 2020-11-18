f45246f5abb38055ab484b615da5f2af638b8f8f
Your Name <morel.florian80@hotmail.fr>
2020-11-18 22:48:58 +0100

    mettre condition

--- exercice4/assets/js/main.js
+++ exercice4/assets/js/main.js
@@ -1,31 +1,42 @@
-let addTasks = document.getElementById('addTasks');
-let toDoContainer = document.getElementById('toDoContainer');
-let inputTasks = document.getElementById('inputTasks');
-let number = document.getElementById('count');
-let count = parseInt(number.innerText)
+var addTasks = document.getElementById('addTasks');
+var toDoContainer = document.getElementById('toDoContainer');
+var inputTasks = document.getElementById('inputTasks');
+var number = document.getElementById('count');
+var count = parseInt(number.innerText)
 
-addTasks.addEventListener('click', function () {
+addTasks.addEventListener('click', function() {
     var liste = document.createElement('li');
-    
+
     liste.classList.add('liste-styling');
-    liste.innerText = inputTasks.value;
+
     // on va ajouter un noeud à la fin de la lsite des enfants 
     toDoContainer.appendChild(liste);
 
-    let parentDiv = toDoContainer.parentNode; 
+    if (inputTasks.value === '') {
+        alert('Rien à ajouter ?');
+        e.preventDefault();
+    } else {
+        liste.innerText = inputTasks.value;
+    }
+
+
+    var parentDiv = toDoContainer.parentNode;
     parentDiv.insertBefore(liste, toDoContainer.nextSibling);
 
+
     inputTasks.value = "";
+
+
     count = count + 1;
     number.innerText = count;
-/***************************************** */
+    /***************************************** */
 
 
-    liste.addEventListener('click', function () {
+    liste.addEventListener('click', function() {
         liste.style.textDecoration = "line-through";
 
     })
-    liste.addEventListener('dblclick', function () {
+    liste.addEventListener('dblclick', function() {
         //  La méthode Node.removeChild() retire un nœud enfant de l'arbre DOM et retourne le nœud retiré.
         parentDiv.removeChild(liste);
         count = count - 1;
