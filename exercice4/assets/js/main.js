36263a26ef3ccf67eda426ad5830b51f99f049db
FlorianMorel80 <you@example.com>
2020-11-18 17:20:52 +0100

    finir to do

--- exercice4/assets/js/main.js
+++ exercice4/assets/js/main.js
@@ -6,16 +6,20 @@ let count = parseInt(number.innerText)
 
 addTasks.addEventListener('click', function () {
     var liste = document.createElement('li');
+    
     liste.classList.add('liste-styling');
     liste.innerText = inputTasks.value;
     // on va ajouter un noeud à la fin de la lsite des enfants 
     toDoContainer.appendChild(liste);
 
+    let parentDiv = toDoContainer.parentNode; 
+    parentDiv.insertBefore(liste, toDoContainer.nextSibling);
+
     inputTasks.value = "";
     count = count + 1;
     number.innerText = count;
+/***************************************** */
 
-    // toDoContainer.insertBefore(toDoContainer, liste.nextSibling);
 
     liste.addEventListener('click', function () {
         liste.style.textDecoration = "line-through";
@@ -23,7 +27,7 @@ addTasks.addEventListener('click', function () {
     })
     liste.addEventListener('dblclick', function () {
         //  La méthode Node.removeChild() retire un nœud enfant de l'arbre DOM et retourne le nœud retiré.
-        toDoContainer.removeChild(liste);
+        parentDiv.removeChild(liste);
         count = count - 1;
         number.innerText = count;
     })
