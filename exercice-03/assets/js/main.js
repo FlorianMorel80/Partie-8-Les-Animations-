02197bad27ea98824496d11fb56b1f6c0bf35a29
FlorianMorel80 <you@example.com>
2020-11-18 15:49:11 +0100

    corrigé

--- exercice-03/assets/js/main.js
+++ exercice-03/assets/js/main.js
@@ -23,52 +23,23 @@ let p = document.querySelectorAll('.invisible').forEach(function (r) {
 
 
 
+// Ou
 
 
+//let para = document.querySelectorAll('p');
 
-// Ici on va avoir un objet qui permet de définir l'ensemble des options de notre intersection observer (check MDN : https://developer.mozilla.org/fr/docs/Web/API/Intersection_Observer_API) 
+// window.onscroll = function() {
+//     let nScroll = window.scrollY;
+//     para.forEach(item => {
+//         //item.classList.add('unvisible');
+//         if (nScroll >= (item.offsetTop -100 ) | nScroll >= (document.body.clientHeight - window.window.innerHeight)) {
+//             //item.classList.remove('unvisible');
+//             item.classList.add('visible');
+//         } else { 
+//             item.classList.remove('visible');
+//             //item.classList.add('unvisible');
+//         }
+//     })
 
-
-// const threshold = .1
-// const options = {
-
-
-    // root est l'élément racine qui servir d'affichage et qui va détecter si l'élément est visible ou non
-    
-    
-    // root: null,   
-    
-    //Nul ici car on ne veut pas que l'élément soit visible à l'écran
-
-
-    // rootMargin: '0px', threshold 
-
-    //le threshhold va permettre d'indiquer à quel moment le système s'intersection va être détecté si il est a 1 ça veut dire que l'entièreté de l'élément doit être visible dans l'écran. O.1 pour dire que dè que 10%  de l'événement est visible on va déclencher les choses 
-    
-//   }
-  
-//   const handleIntersect = function (entries, observer) {
-//     entries.forEach(function (entry) {
-//       if (entry.intersectionRatio > threshold) {
-//         entry.target.classList.remove('reveal')
-//         observer.unobserve(entry.target)
-//       }
-      
-//   })
+//     console.log(nScroll + "...." + window.innerHeight +"/"+ document.body.clientHeight);
 // }
-
-  
-// document.documentElement.classList.add('reveal-loaded')
-
-// window.addEventListener("DOMContentLoaded", function () {
-
-
-  //On créeait un nouveau intersectionObserver et ensuite on lui attribut le premier paramètre handleIntersect qui sera la fonction qui sera executé lordque l'élément deviendra visible ou sera masqué de la zone. Deuxème paramètre, l'objezt qui va contenir les options 
-
-
-//   const observer = new IntersectionObserver(handleIntersect, options)
-//   const targets = document.querySelectorAll('.reveal')
-//   targets.forEach(function (target) {
-//     observer.observe(target)
-//   })
-// })
